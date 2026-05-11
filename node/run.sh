e968b82e029b5a9701c98434c69ffb20c0fe7ede
Kevin Albertson <kevin.albertson@mongodb.com>
2026-05-11 15:57:23 -0400

    node/run.sh: use npm ci and drop --silent flag

--- node/run.sh
+++ node/run.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 if [ -n "${LOCAL_NODE_DRIVER_PATH:-}" ]; then
-    npm install "$LOCAL_NODE_DRIVER_PATH" --silent
+    npm install "$LOCAL_NODE_DRIVER_PATH" >/dev/null
 else
-    npm install --silent
+    npm ci >/dev/null
 fi
 
 node hello.js
